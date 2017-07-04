c1c7a4bfd83d18d7ceffeacf9433b9c66d218e03
Sam Jacobson <samuel.c.jacobson@gmail.com>
2017-07-04 08:58:51 -0400

    stretch task

--- FortuneTeller/Program.cs
+++ FortuneTeller/Program.cs
@@ -12,103 +12,127 @@ namespace FortuneTeller
         {
             //Part 1 - asking for user input
             Console.WriteLine("Welcome to the Fortune Teller! Enter the below information to learn your future.");
+            Console.WriteLine("At any time you may enter \"Quit\" to close the program.");
 
-            Console.WriteLine("What is your first name?");
+            Console.WriteLine("\nWhat is your first name?");
             string firstName = Console.ReadLine();
+            if (firstName.Equals("Quit", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Nobody likes a quitter...");
+                Environment.Exit(0);
+            }
 
             Console.WriteLine("What is your last name?");
             string lastName = Console.ReadLine();
+            if (lastName.Equals("Quit", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Nobody likes a quitter...");
+                Environment.Exit(0);
+            }
 
             Console.WriteLine("How old are you?");
-            int age = int.Parse(Console.ReadLine());
+            int age = int.Parse(Console.ReadLine()); //Unable to figure out how to accept "quit" (a string) when asking for an int
 
             Console.WriteLine("What is your birth month? Please enter as a digit.");
-            int birthMonth = int.Parse(Console.ReadLine());
+                int birthMonth = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("What is your favortie ROYGBIV color? If you do not know what ROYGBIV is, enter \"Help\"");
-            string color = Console.ReadLine();
-            if (color == "Help")
-            {
-                Console.WriteLine("ROYGBIV is an acronym for the following colors: \nRed \nOrange \nYellow \nGreen \nBlue \nIndigo \nViolet");
-            }
+            Console.WriteLine("What is your favorite ROYGBIV color? If you do not know what ROYGBIV is, enter \"Help\"");
+                string color = Console.ReadLine();
+                if (color.Equals("Help", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("ROYGBIV is an acronym for the following colors: \nRed \nOrange \nYellow \nGreen \nBlue \nIndigo \nViolet");
+                    Console.WriteLine("\nPlease enter your choice from the above.");
+                    color = Console.ReadLine();
+                }
+                if (color.Equals("Quit", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Nobody likes a quitter...");
+                    Environment.Exit(0);
+                }
 
             Console.WriteLine("How many siblings do you have?");
-            int siblings = int.Parse(Console.ReadLine());
+                int siblings = int.Parse(Console.ReadLine());
 
-            //Part 2 - calculations on user input
-            //Declare variables
-            int retirement;
-            string vacation;
-            string transportation;
-            double funds;
 
-            //Values of variables determined based upon conditionals
-            if (age % 2 == 0)
-            {
-                retirement = 10;
-            }
-            else
-            {
-                retirement = 5;
-            }
+                //Part 2 - calculations on user input
+                //Declare variables
+                int retirement;
+                string vacation;
+                string transportation;
+                double funds;
 
-            if (siblings == 0)
-            {
-                vacation = "Tahiti";
-            }
-            else if (siblings == 1)
-            {
-                vacation = "Tuscany";
-            }
-            else if (siblings == 2)
-            {
-                vacation = "Thailand";
-            }
-            else if (siblings >= 3)
-            {
-                vacation = "Zanzibar";
-            }
-            else
-            {
-                vacation = "a refrigertor box";
-            }
+                //Values of variables determined based upon conditionals
+                //Retirement age
+                if (age % 2 == 0)
+                {
+                    retirement = 10;
+                }
+                else
+                {
+                    retirement = 5;
+                }
 
-            if (color == "red" || color == "violet")
-            {
-                transportation = "sailboat";
-            }
-            else if (color == "orange" || color == "blue")
-            {
-                transportation = "Ferrari";
-            }
-            else if (color == "yellow" || color == "green")
-            {
-                transportation = "stagecoach";
-            }
-            else
-            {
-                transportation = "Lear jet";
-            }
+                //Vacation house
+                if (siblings == 0)
+                {
+                    vacation = "Tahiti";
+                }
+                else if (siblings == 1)
+                {
+                    vacation = "Tuscany";
+                }
+                else if (siblings == 2)
+                {
+                    vacation = "Thailand";
+                }
+                else if (siblings >= 3)
+                {
+                    vacation = "Zanzibar";
+                }
+                else
+                {
+                    vacation = "a refrigertor box";
+                }
 
-            if (birthMonth >=1 && birthMonth <= 4)
-            {
-                funds = 100d;
-            }
-            else if (birthMonth >=5 && birthMonth <=8)
-            {
-                funds = 50000d;
-            }
-            else if (birthMonth >=9 && birthMonth <=12)
-            {
-                funds = 9000d;
-            }
-            else
-            {
-                funds = 0.00d;
-            }
+                //Transportation mode
+                if (color.Equals("red", StringComparison.OrdinalIgnoreCase) || color.Equals("violet", StringComparison.OrdinalIgnoreCase))
+                {
+                    transportation = "sailboat";
+                }
+                else if (color.Equals("orange", StringComparison.OrdinalIgnoreCase) || color.Equals("blue", StringComparison.OrdinalIgnoreCase))
+                {
+                    transportation = "Ferrari";
+                }
+                else if (color.Equals("yellow", StringComparison.OrdinalIgnoreCase) || color.Equals("green", StringComparison.OrdinalIgnoreCase))
+                {
+                    transportation = "stagecoach";
+                }
+                else
+                {
+                    transportation = "Lear jet";
+                }
 
-            //Part 3 - display fortune based upon above calculations
-            Console.WriteLine(firstName + " " + lastName + " will retire in " + retirement + " years with $" + funds + " in the bank, a vacation home in " + vacation + ", and a " + transportation + ".");
+                //Bank account
+                if (birthMonth >= 1 && birthMonth <= 4)
+                {
+                    funds = 100.00d;
+                }
+                else if (birthMonth >= 5 && birthMonth <= 8)
+                {
+                    funds = 50000.00d;
+                }
+                else if (birthMonth >= 9 && birthMonth <= 12)
+                {
+                    funds = 9000.00d;
+                }
+                else
+                {
+                    funds = 0.00d;
+                }
+
+
+                //Part 3 - display fortune based upon above calculations
+                Console.WriteLine(firstName + " " + lastName + " will retire in " + retirement + " years with $" + funds + " in the bank, a vacation home in " + vacation + ", and a " + transportation + ".");
+            }
         }
     }
-}
